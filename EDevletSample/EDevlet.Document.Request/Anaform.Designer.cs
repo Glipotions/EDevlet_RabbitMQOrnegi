8a536d0164495c3d3bbe4c311cc2920a4b4e59ff
Glipotions <hamzakavak1905@gmail.com>
2022-08-22 00:46:07 +0300

    Proje dosyası ekle.

---
+++ EDevletSample/EDevlet.Document.Request/Anaform.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace EDevlet.Document.Request
+{
+    partial class Anaform
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtConnectionString = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnCreateDocument = new System.Windows.Forms.Button();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtConnectionString);
+            this.groupBox1.Controls.Add(this.btnConnect);
+            this.groupBox1.Location = new System.Drawing.Point(3, 11);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(570, 67);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Connection";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ConnectionString";
+            // 
+            // txtConnectionString
+            // 
+            this.txtConnectionString.Location = new System.Drawing.Point(131, 22);
+            this.txtConnectionString.Name = "txtConnectionString";
+            this.txtConnectionString.Size = new System.Drawing.Size(277, 20);
+            this.txtConnectionString.TabIndex = 1;
+            this.txtConnectionString.Text = "amqp://guest:guest@localhost:5672";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(426, 22);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(118, 29);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnCreateDocument
+            // 
+            this.btnCreateDocument.Location = new System.Drawing.Point(184, 141);
+            this.btnCreateDocument.Name = "btnCreateDocument";
+            this.btnCreateDocument.Size = new System.Drawing.Size(181, 80);
+            this.btnCreateDocument.TabIndex = 1;
+            this.btnCreateDocument.Text = "Create Document";
+            this.btnCreateDocument.UseVisualStyleBackColor = true;
+            this.btnCreateDocument.Click += new System.EventHandler(this.btnCreateDocument_Click);
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(3, 258);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(570, 222);
+            this.txtLog.TabIndex = 2;
+            // 
+            // Anaform
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(581, 492);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.btnCreateDocument);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Anaform";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtConnectionString;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnCreateDocument;
+        private System.Windows.Forms.TextBox txtLog;
+    }
+}
+
---
+++ EDevletSample/EDevlet.Document.Request/Anaform.cs
@@ -0,0 +1,120 @@
+﻿using EDevlet.Document.CommonF;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EDevlet.Document.Request
+{
+    public partial class Anaform : Form
+    {
+        IConnection connection;
+        private readonly string createDocument = "create_document_queue";
+        private readonly string documentCreated = "document_created_queue";
+        private readonly string documentCreateExchange = "document_create_exchange";
+        IModel _channel;
+        IModel channel => _channel ?? (_channel = GetChannel());
+
+
+
+        public Anaform()
+        {
+            InitializeComponent();
+        }
+        private void btnCreateDocument_Click(object sender, EventArgs e)
+        {
+            var model = new CreateDocumentModel()
+            {
+                UserId = 1,
+                DocumentType = DocumentType.Pdf,
+
+            };
+
+            WriteToQueue(createDocument, model);
+
+            var consumerEvent = new EventingBasicConsumer(channel);
+
+            consumerEvent.Received += (ch, ea) =>
+            {
+                var modelReceived = JsonConvert.DeserializeObject<CreateDocumentModel>(Encoding.UTF8.GetString(ea.Body.ToArray()));
+                AddLog($"Received Data Url : {modelReceived.Uri}");
+            };
+
+            //consumerEvent.Received += ConsumerEvent_Received;
+
+            channel.BasicConsume(documentCreated, true, consumerEvent);
+        }
+
+        //private void ConsumerEvent_Received(object sender, BasicDeliverEventArgs e)
+        //{
+        //    var modelReceived = JsonConvert.DeserializeObject<CreateDocumentModel>(Encoding.UTF8.GetString(e.Body.ToArray()));
+        //    AddLog($"Received Data Url : {modelReceived.Uri}");
+        //}
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if (connection == null || !connection.IsOpen)
+            {
+                connection = GetConnection();
+            }
+            btnCreateDocument.Enabled = true;
+
+            channel.ExchangeDeclare(documentCreateExchange, "direct");
+            channel.QueueDeclare(createDocument, false, false, false);
+            channel.QueueBind(createDocument, documentCreateExchange, createDocument);
+
+            channel.QueueDeclare(documentCreated, false, false, false);
+            channel.QueueBind(documentCreated, documentCreateExchange, documentCreated);
+
+
+
+            AddLog("Connection is Open now");
+        }
+
+        private void WriteToQueue(string queueName, CreateDocumentModel model)
+        {
+            var messageArr = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(model));
+
+            channel.BasicPublish(documentCreateExchange, queueName, null, messageArr);
+
+            AddLog("Message Published");
+        }
+
+
+        private IModel GetChannel()
+        {
+            return connection.CreateModel();
+        }
+        private IConnection GetConnection()
+        {
+            var createConFactory = new ConnectionFactory()
+            {
+                Uri = new Uri(txtConnectionString.Text.Trim())
+            };
+
+            return createConFactory.CreateConnection();
+        }
+
+        private void AddLog(string logStr)
+        {
+            if (txtLog.InvokeRequired)
+            {
+                txtLog.Invoke(new Action(() => AddLog(logStr)));
+                return;
+            }
+            logStr = $"[{DateTime.Now:dd.MM.yyyy HH:mm:ss}] - {logStr}";
+            txtLog.AppendText($"{logStr} \n");
+
+            txtLog.SelectionStart = txtLog.Text.Length;
+            txtLog.ScrollToCaret();
+        }
+    }
+}
